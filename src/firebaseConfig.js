1be2b4ac3829aa7efd45485acda2c12261535693
akinawa <octaviosanchezc123@gmail.com>
2023-08-12 18:03:12 -0300

    arreglo de Api

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE,
-    messagingSenderId: import.meta.env.VITE_MESSAGING,
-    appId: import.meta.env.VITE_APPID,
+    apiKey: "AIzaSyD4reMobtFUDYhXCHfnnRqvxdUIUu_37Ic",
+    authDomain: "proyectofinal-com43290.firebaseapp.com",
+    projectId: "proyectofinal-com43290",
+    storageBucket: "proyectofinal-com43290.appspot.com",
+    messagingSenderId: "1069349671553",
+    appId: "1:1069349671553:web:19719c1c15a46780aa7380"
 };
 
 // Initialize Firebase
